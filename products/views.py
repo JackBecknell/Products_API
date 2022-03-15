886a155b50a0c88578aad1af51ed8e8a0fdf1fbf
JackBecknell <52moosefit@gmail.com>
2022-03-15 17:01:43 -0400

    Completed DELETE functionality

--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from .serializers import ProductSerializer
 from .models import Product
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST',])
 def products_list(request):
     if request.method == 'GET':
         products = Product.objects.all()
@@ -17,7 +17,7 @@ def products_list(request):
         serializer.save()
         return Response(serializer.data, status = status.HTTP_201_CREATED)
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def products_detail(request, pk):  
     product = get_object_or_404(Product, pk=pk)
     if request.method == 'GET':
@@ -28,5 +28,8 @@ def products_detail(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
