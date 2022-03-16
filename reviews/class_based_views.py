5981e8586ac066cca5c2e2b9bcd706a3a9c2ac5b
JackBecknell <52moosefit@gmail.com>
2022-03-16 16:58:15 -0400

    Created Update, Delete, Get by ID, and Get by FK funcitonality for the Reviews application

--- reviews/class_based_views.py
+++ reviews/class_based_views.py
@@ -19,6 +19,33 @@ class ReviewList(APIView):
         return Response(serializer.data, status = status.HTTP_201_CREATED)
 
 class ReviewDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Review.objects.get(pk=pk)
+        except Review.DoesNotExist:
+            raise Http404
+    #the Gets_object function relies on the get function to return a single Review by pk
+    def get(self, request, pk, format=None):
+        review = self.get_object(pk)
+        serializer = ReviewSerializer(review)
+        return Response(serializer.data)
+    
+    #Updates single reivew by pk
+    def put(self, request, pk, format=None):
+        review = self.get_object(pk)
+        serializer = ReviewSerializer(review, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    #Deletes single review by pk
+    def delete(self, request, pk, format=None):
+        review = self.get_object(pk)
+        review.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class ReviewForeignKey(APIView): 
     def get(self, request, fk, format=None):
         reviews = Review.objects.filter(product = fk)
         serializer = ReviewSerializer(reviews, many=True)
--- reviews/urls.py
+++ reviews/urls.py
@@ -5,5 +5,6 @@ from . import class_based_views
 urlpatterns = [
     path('', class_based_views.ReviewList.as_view()),
     #will add a future path here for when pk is passed as well
-    path('<int:fk>/', class_based_views.ReviewDetail.as_view())
+    path('<int:pk>/', class_based_views.ReviewDetail.as_view()),
+    path('fk/<int:fk>/', class_based_views.ReviewForeignKey.as_view())
 ]
